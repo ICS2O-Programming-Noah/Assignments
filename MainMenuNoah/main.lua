f4b61cf5b6f871292860c364f46355a275bbe418
Noah Sabbagh <61517057+s462947@users.noreply.github.com>
2020-05-28 10:37:25 -0400

    Added Main Menu

---
+++ MainMenuNoah/main.lua
@@ -0,0 +1,23 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+-- Created by: Noah Sabbagh
+-- Date: May 25, 2020
+-- Description: This calls the splash screen of the app to load itself.
+-----------------------------------------------------------------------------------------
+
+-- Hiding Status Bar
+-- HIDE THE STATUS BAR
+
+-----------------------------------------------------------------------------------------
+
+-- Use composer library
+local composer = require( "composer" )
+
+-----------------------------------------------------------------------------------------
+
+-- Go to the intro screen
+composer.gotoScene( "splash_screen" )
+
+-- hide the status Bar
+display.setStatusBar(display.HiddenStatusBar)
